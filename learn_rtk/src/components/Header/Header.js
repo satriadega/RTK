ada3e8ca3ebd1434ae5a9eb5ee8ea2026abf2b05
huntr <satria.pandega@gmail.com>
2022-11-15 17:13:43 +0700

    createAsyncThunk

--- learn_rtk/src/components/Header/Header.js
+++ learn_rtk/src/components/Header/Header.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { fetchRandomUserData, logout } from "../../store/slices/authSlice";
 
 const Header = () => {
+    const dispatch = useDispatch();
+
     return (
         <div className="header">
             <h1>Personal Library</h1>
+            <button onClick={() => dispatch(logout())} className="logout-btn">
+                Logout
+            </button>
+            <button
+                onClick={() => dispatch(fetchRandomUserData())}
+                className="user-btn"
+            >
+                Get User Data
+            </button>
         </div>
     );
 };
--- learn_rtk/src/store/store.js
+++ learn_rtk/src/store/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookReducer from "./slices/bookSlice";
+import authReducer from "./slices/authSlice";
 
 // dummy reducer will be replaced when the real reducers are created
 const store = configureStore({
-    reducer: { books: bookReducer },
+    reducer: { books: bookReducer, auth: authReducer },
 });
 
 export default store;
